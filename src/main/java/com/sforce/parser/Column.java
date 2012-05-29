fb11dd0eeeb252a3f378e0719d858102257f13fc
Elliot Chen <chenensign@gmail.com>
2012-05-29 18:44:59 +0800

    update log4j.xml

---
+++ src/main/java/com/sforce/parser/Column.java
@@ -0,0 +1,29 @@
+package com.sforce.parser;
+
+public class Column {
+	private int index;
+	private String name;
+	private String sfName;
+	
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSfName() {
+		return sfName;
+	}
+	public void setSfName(String sfName) {
+		this.sfName = sfName;
+	}
+	
+	
+}
