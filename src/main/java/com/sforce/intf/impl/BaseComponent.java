da21f36555afb165f9d3e401b798db9001589316
Elliot Chen <chenensign@gmail.com>
2012-06-11 18:02:08 +0800

    add library

--- src/main/java/com/sforce/intf/impl/BaseComponent.java
+++ src/main/java/com/sforce/intf/impl/BaseComponent.java
@@ -16,17 +16,40 @@ public class BaseComponent implements Component {
 	}
 	@Override
 	public Boolean init() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
 	public Boolean trigger() {
-		return null;
+		this.status = Status.Reading;
+		try {
+			this.receiver.receive();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		this.status = Status.Sending;
+		try {
+			this.sender.send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		this.status = Status.Available;
+		return true;
 	}
 	@Override
 	public Boolean isAvailable() {
 		return Status.Available.equals(status);
 	}
-	
+	public Receiver getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(Receiver receiver) {
+		this.receiver = receiver;
+	}
+	public Sender getSender() {
+		return sender;
+	}
+	public void setSender(Sender sender) {
+		this.sender = sender;
+	}
 	
 }
