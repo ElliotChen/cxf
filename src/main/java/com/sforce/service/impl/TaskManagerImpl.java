ba6f3ec72fe6a10beebe8e8889d135d53aba417f
Elliot <elliot@ubuntu.home>
2012-06-21 06:19:15 +0800

    update

--- src/main/java/com/sforce/service/impl/TaskManagerImpl.java
+++ src/main/java/com/sforce/service/impl/TaskManagerImpl.java
@@ -1,7 +1,13 @@
 package com.sforce.service.impl;
 
+import org.springframework.core.task.TaskExecutor;
+
 import com.sforce.service.TaskManager;
 
 public class TaskManagerImpl implements TaskManager {
-
+	private TaskExecutor taskExecutor;
+	
+	public void arrange(Runnable task) {
+		this.taskExecutor.execute(task);
+	}
 }
